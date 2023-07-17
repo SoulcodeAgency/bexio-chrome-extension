ecc0019f2b7a38eea6c9e9c5c095d3c3435fbfca
Fabian Gander <fabian.gander@soulcode.ch>
2023-07-18 00:08:52 +0200

    Clicking the tracking button will make it checked, until clicked again.

--- sidePanel-import/src/components/ImportEntries/ImportEntriesTableCell.tsx
+++ sidePanel-import/src/components/ImportEntries/ImportEntriesTableCell.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type ImportEntriesTableCellProps = {
   columnHeader: string;
   fieldValue: string;
@@ -8,13 +10,25 @@ const ImportEntriesTableCell = (props: ImportEntriesTableCellProps) => {
   const emptyDateRegex = /^0:00:00$/;
   const columnIsATrackingDay = dateRegex.test(props.columnHeader);
   const entryIsEmpty = emptyDateRegex.test(props.fieldValue);
+  const [clicked, setClicked] = useState(false);
+
+  function clickHandler() {
+    props.onButtonClick();
+    setClicked(true);
+  }
+
+  // TODO: we could even save this state into a new store (using date and entryIndex)
+  let button = <button onClick={clickHandler}>▶️</button>;
+  if (clicked) {
+    button = <button onClick={() => setClicked(false)}>✅</button>;
+  }
 
   let tableCell = <td>{props.fieldValue}</td>;
   if (columnIsATrackingDay && !entryIsEmpty) {
     tableCell = (
       <td>
         {props.fieldValue}
-        <button onClick={props.onButtonClick}>▶️</button>
+        {button}
       </td>
     );
   } else if (entryIsEmpty) {
