26b614917b9677662bf3c7a87d2c480362fbe4d6
Fabian Gander <fabian.gander@soulcode.ch>
2023-07-21 18:08:57 +0200

    Introducing AntDesign, adding some tabs and styling

--- sidePanel-import/src/App.tsx
+++ sidePanel-import/src/App.tsx
@@ -1,32 +1,30 @@
 import "./App.css";
+import { Tabs } from "antd";
+import type { TabsProps } from "antd";
+import TemplateProvider from "./TemplateProvider";
 import TemplateEntries from "./components/TemplateEntries/TemplateEntries";
 import ImportEntries from "./components/ImportEntries/ImportEntries";
-import { TemplateContext } from "./TemplateContext";
-import { useEffect, useState } from "react";
-import { loadTemplates } from "@shared/chromeStorageTemplateEntries";
-import { TemplateEntry } from "@shared/types";
 
 function App() {
-  const [templates, setTemplates] = useState<TemplateEntry[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const templateEntries = await loadTemplates();
-      setTemplates(templateEntries);
-    };
-
-    fetchData();
-  }, []);
+  const items: TabsProps["items"] = [
+    {
+      key: "1",
+      label: `Templates`,
+      children: <TemplateEntries />,
+    },
+    {
+      key: "2",
+      label: `Bexio import`,
+      children: <ImportEntries />,
+    },
+  ];
 
   return (
-    <>
-      <TemplateContext.Provider value={templates}>
-        <div style={{ textAlign: "left" }}>
-          <TemplateEntries />
-          <ImportEntries />
-        </div>
-      </TemplateContext.Provider>
-    </>
+    <TemplateProvider>
+      <div style={{ textAlign: "left" }}>
+        <Tabs defaultActiveKey="1" items={items} />
+      </div>
+    </TemplateProvider>
   );
 }
 
--- sidePanel-import/src/main.tsx
+++ sidePanel-import/src/main.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./index.scss";
 import soulcodeLogo from "./assets/soulcode-logo.png";
 import * as packageInfo from "../../package.json";
+import { ConfigProvider } from "antd";
 // eslint-disable-next-line react-refresh/only-export-components
 const VERSION = packageInfo.version;
 const copyrightSymbol = "\u00A9";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <img className="soulcode-logo" src={soulcodeLogo} alt="Soulcode logo" />
-    <App />
-    <a href="https://soulcode.ch" target="_blank">
-      {copyrightSymbol} Soulcode AG - {VERSION}
-    </a>
+    <div style={{ position: "relative" }}>
+      <img className="soulcode-logo" src={soulcodeLogo} alt="Soulcode logo" />
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#3176b4",
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
+      <a href="https://soulcode.ch" target="_blank">
+        {copyrightSymbol} Soulcode AG - {VERSION}
+      </a>
+    </div>
   </React.StrictMode>
 );
