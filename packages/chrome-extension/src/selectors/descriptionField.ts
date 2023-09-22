681c05b3fcf20539d28fc7d5bb4fef8f943f3d7d
Fabian Gander <fabian.gander@soulcode.ch>
2023-09-22 20:09:07 +0200

    fix(iframe) Fixed accessing iframe information too early
    Migrated to a getter function

--- packages/chrome-extension/src/selectors/descriptionField.ts
+++ packages/chrome-extension/src/selectors/descriptionField.ts
@@ -1,4 +1,5 @@
 export const descriptionFieldId = "#tinymce";
 // We need to go through the iframe to access the description field
 const iframe = document.querySelector("#monitoring_text_ifr") as HTMLIFrameElement;
-export const descriptionField = iframe.contentWindow?.document.querySelector(descriptionFieldId) as HTMLInputElement | undefined;
+// Don't access this on load, it can lead to problems, thats why we have a getter function here
+export const getDescriptionField = () => iframe?.contentWindow?.document.querySelector(descriptionFieldId) as HTMLInputElement | undefined;
--- packages/chrome-extension/src/utils/triggerDescription.ts
+++ packages/chrome-extension/src/utils/triggerDescription.ts
@@ -1,7 +1,8 @@
-import { descriptionField } from "../selectors/descriptionField";
+import { getDescriptionField } from "../selectors/descriptionField";
 
 // Trigger description field
 async function triggerDescription(value: string) {
+    const descriptionField = getDescriptionField();
     if (descriptionField) {
         descriptionField.textContent = value;
     }
