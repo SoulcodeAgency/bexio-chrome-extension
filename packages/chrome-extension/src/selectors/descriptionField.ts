4e63efd2e8b18415780407bd46442e1691924656
Fabian Gander <fabian.gander@soulcode.ch>
2023-11-12 15:20:57 +0100

    fix(descriptionField) Fixed possible issues with finding the iframe

--- packages/chrome-extension/src/selectors/descriptionField.ts
+++ packages/chrome-extension/src/selectors/descriptionField.ts
@@ -1,5 +1,13 @@
 export const descriptionFieldId = "#tinymce";
-// We need to go through the iframe to access the description field
-const iframe = document.querySelector("#monitoring_text_ifr") as HTMLIFrameElement;
 // Don't access this on load, it can lead to problems, thats why we have a getter function here
-export const getDescriptionField = () => iframe?.contentWindow?.document.querySelector(descriptionFieldId) as HTMLInputElement | undefined;
+export const getDescriptionField = () => {
+    // We need to go through the iframe to access the description field
+    const iframe = document.querySelector("#monitoring_text_ifr") as HTMLIFrameElement;
+    const descriptionInputfield = iframe?.contentWindow?.document.querySelector(descriptionFieldId) as HTMLInputElement | undefined;
+    if (descriptionInputfield) {
+        return descriptionInputfield;
+    } else {
+        console.error(iframe, descriptionInputfield);
+        throw new Error("Description field not found");
+    }
+}
