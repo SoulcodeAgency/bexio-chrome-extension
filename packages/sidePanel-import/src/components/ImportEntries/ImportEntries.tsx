7d061e799eb040dc15f284e2ca8e92c2a9fea21e
Fabian Gander <fabian.gander@soulcode.ch>
2023-12-22 13:42:00 +0100

    feat(AutoMapper) Extracting mapper function v1

--- packages/sidePanel-import/src/components/ImportEntries/ImportEntries.tsx
+++ packages/sidePanel-import/src/components/ImportEntries/ImportEntries.tsx
@@ -12,6 +12,7 @@ import {
 } from "@bexio-chrome-extension/shared/chromeStorageSettings";
 import { EntryExchangeData } from "@bexio-chrome-extension/shared/types";
 import { autoMapTemplatesV3 } from "./AutoMapTemplatesV3";
+import { autoMapTemplatesV1 } from "./AutoMapTemplatesV1";
 
 export type ImportRow = string[];
 export type ImportData = ImportRow[];
@@ -193,12 +194,6 @@ function ImportEntries() {
     setTabs(["apply"]);
   }
 
-  // Disabled for the moment - checking need
-  // function reloadImportData() {
-  //   setTabs(["apply"]);
-  //   loadImportData();
-  // }
-
   const tagColumnIndexes = importHeader.reduce(
     (acc: number[], column, index) => {
       if (column.startsWith("Tag")) {
@@ -209,122 +204,13 @@ function ImportEntries() {
     []
   );
 
-  function autoMapTemplates() {
-    const importTemplateAssignment: string[] = [];
-    importData.forEach((row, rowIndex) => {
-      console.groupCollapsed(`Entry ${rowIndex + 1}`);
-      const mappingResult: { [key: string]: number } = {};
-      const tagColumnsContent = tagColumnIndexes.map((index) => row[index]);
-      // Split content of every tag column by space to search for every word
-      tagColumnsContent.forEach((tagColumn, columnIndex) => {
-        const tagWords = tagColumn.match(/[a-zA-Z0-9]+/g);
-        console.log(
-          `Identified words in ${importHeader[columnIndex]}:`,
-          tagWords
-        );
-        // Count how many times each word occurs in the templateEntries and count them up
-        tagWords?.length &&
-          tagWords.map((tagWord) => {
-            if (tagWord === "") return;
-            tagWord = tagWord.toLowerCase();
-            templateEntries.map((entry) => {
-              let matches = 0;
-              // Give points for the following columns if they match the tagWord
-              entry.templateName.toLowerCase().includes(tagWord)
-                ? matches++
-                : null;
-              entry.contact
-                ? entry.contact.toLowerCase().includes(tagWord)
-                  ? matches++
-                  : null
-                : null;
-              entry.project
-                ? entry.project.toLowerCase().includes(tagWord)
-                  ? matches++
-                  : null
-                : null;
-              entry.package
-                ? entry.package.toLowerCase().includes(tagWord)
-                  ? matches++
-                  : null
-                : null;
-              entry.contactPerson
-                ? entry.contactPerson.toLowerCase().includes(tagWord)
-                  ? matches++
-                  : null
-                : null;
-              entry.keywords
-                ? entry.keywords.toLowerCase().includes(tagWord)
-                  ? matches++
-                  : null
-                : null;
-
-              // TODO: maybe we should turn around the weight
-              // Make the count weight increase for every column: For the first tag column *1, second tag column *2, third tag column *3 etc.
-              const countIncrease = matches * (columnIndex + 1);
-              mappingResult[entry.id] =
-                (mappingResult[entry.id] ?? 0) + countIncrease;
-
-              // TODO: to better debug the points, we should change the mappingResult object to an array of objects, and add the templateName to the object
-              // if (matches)
-              //   console.warn(
-              //     tagWord,
-              //     "made",
-              //     countIncrease,
-              //     "points on " + entry.templateName
-              //   );
-            });
-          });
-      });
-
-      // Get the key(template id) of the mappingResult which has the highest value
-      const templateId = Object.keys(mappingResult).reduce((a, b) =>
-        mappingResult[a] > mappingResult[b] ? a : b
-      );
-      const templateName = getTemplateName(
-        templateEntries.find((entry) => entry.id === templateId)!
-      );
-
-      // Check if there is only 1 highest value, otherwise we do not auto map and leave the decision to the user
-      const highestValue = Math.max(...Object.values(mappingResult));
-      const highestValueCount = Object.values(mappingResult).filter(
-        (value) => value === highestValue
-      ).length;
-
-      if (highestValueCount === 1) {
-        // We have a winner! Assign the template id to the row
-        importTemplateAssignment[rowIndex] = templateId;
-        console.log(
-          "Auto mapping template: " + "TemplateId: " + templateId,
-          +"templateName: " + templateName
-        );
-      } else {
-        // No clear winner, leave empty
-        console.log("Auto mapping template: No clear winner!");
-      }
-
-      // Next stuff is only needed for nice debugging table of points
-
-      // Map over the mappingResult, and replace the index with the template name from templateEntries
-      Object.keys(mappingResult).map((templateId) => {
-        const templateName = getTemplateName(
-          templateEntries.find((entry) => entry.id === templateId)!
-        );
-        mappingResult[templateName ?? ""] = mappingResult[templateId];
-        delete mappingResult[templateId];
-      });
-
-      // Sort the mappingResult by highest value
-      const sortedMappingResult: { [key: string]: number } = {};
-      Object.keys(mappingResult)
-        .sort((a, b) => mappingResult[b] - mappingResult[a])
-        .forEach((key) => {
-          sortedMappingResult[key] = mappingResult[key];
-        });
-      console.table(sortedMappingResult);
-
-      console.groupEnd();
-    });
+  function callAutoMapTemplatesV1() {
+    const importTemplateAssignment = autoMapTemplatesV1(
+      importData,
+      templateEntries,
+      importHeader,
+      tagColumnIndexes
+    );
 
     // Save the auto mapped templates
     setImportTemplates(importTemplateAssignment);
@@ -416,11 +302,11 @@ function ImportEntries() {
   const importDataHTML = (
     <div className="content">
       <Tooltip title="This will try to select the right template for your time entries">
-        <Button type="primary" onClick={autoMapTemplates}>
-          Auto map templates
+        <Button type="default" onClick={callAutoMapTemplatesV1}>
+          Auto map templates v1
         </Button>
       </Tooltip>
-      <Tooltip title="New algorithm which weights the bexio fields, not the Tag columns">
+      <Tooltip title="Based on v2, but further weights exact word matches">
         <Button type="primary" onClick={callAutoMapTemplatesV3}>
           Auto map templates v3
         </Button>
@@ -504,10 +390,6 @@ function ImportEntries() {
       <br />
       <br />
       <div>
-        {/* TODO This button is probably more confusing than helping */}
-        {/* <Button type="dashed" onClick={reloadImportData}>
-          Reload saved data
-        </Button> */}
         <Button danger onClick={removeImportData}>
           Delete saved data
         </Button>
