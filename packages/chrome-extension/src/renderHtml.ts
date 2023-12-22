4ad8a8d04c792f060c3907fc53ed0779881cd2ee
Fabian Gander <fabian.gander@soulcode.ch>
2023-12-22 23:17:04 +0100

    feat(Logo) Loading icon update and zoomed logo32 to improve visibility

--- packages/chrome-extension/src/renderHtml.ts
+++ packages/chrome-extension/src/renderHtml.ts
@@ -6,29 +6,40 @@ import readFormData from "./utils/readFormData";
 
 // Renders all the html code for placing buttons to interact with
 async function renderHtml(templateEntries) {
-    // Add some buttons into the page
-    let buttons = '';
-    if (templateEntries) {
-        templateEntries.map(entry => buttons += `<button type="button" id="${entry.id}" class="entry btn btn-info template-button" style="width: 100%; margin-bottom: 2px; text-align: left;">${getTemplateName(entry)}</button>`);
-    }
+  // Add some buttons into the page
+  let buttons = "";
+  if (templateEntries) {
+    templateEntries.map(
+      (entry) =>
+        (buttons += `<button type="button" id="${
+          entry.id
+        }" class="entry btn btn-info template-button" style="width: 100%; margin-bottom: 2px; text-align: left;">${getTemplateName(
+          entry
+        )}</button>`)
+    );
+  }
 
-    // Remove the templates HTML, if it already exists
-    const templates = document.getElementById("SoulcodeExtensionTemplates");
-    if (templates) {
-        templates.remove();
-    }
+  // Remove the templates HTML, if it already exists
+  const templates = document.getElementById("SoulcodeExtensionTemplates");
+  if (templates) {
+    templates.remove();
+  }
 
-    const templatePlacement = document.getElementById("pr_package")?.parentNode?.parentNode?.parentNode as HTMLElement;
-    const htmlTemplateButtons = `<div id="bexioTimetrackingTemplates-entries" style="column-count: 2; column-fill: balance;">
+  const templatePlacement = document.getElementById("pr_package")?.parentNode
+    ?.parentNode?.parentNode as HTMLElement;
+  const htmlTemplateButtons = `<div id="bexioTimetrackingTemplates-entries" style="column-count: 2; column-fill: balance;">
         ${buttons}
     </div>`;
-    const htmlActions = `<div id="SoulcodeExtensionActions" style="margin-left: 4px; margin-bottom: 5px;">
+  const htmlActions = `<div id="SoulcodeExtensionActions" style="margin-left: 4px; margin-bottom: 5px;">
             <button type="button" id="AddNewTemplate" class="btn btn-info">+ Add as Template</button>
             <button type="button" id="DeleteTemplate" class="btn btn-danger">Delete Template</button>
         </div>`;
 
-    // Place the html into the DOM
-    templatePlacement.insertAdjacentHTML("beforeend", `<div id="SoulcodeExtensionTemplates" class="row-fluid">
+  // Place the html into the DOM
+  const logoPath = chrome.runtime.getURL("assets/logo_orig.png");
+  templatePlacement.insertAdjacentHTML(
+    "beforeend",
+    `<div id="SoulcodeExtensionTemplates" class="row-fluid">
         <hr>
         <div class="bx-formular-header" style="display: flex; justify-content: space-between">
             <h2 title="Last update: ${DATE}">
@@ -49,32 +60,48 @@ async function renderHtml(templateEntries) {
         display: flex;
         justify-content: center;
         align-items: center;
-        font-size: 10vw;
+        font-size: 5rem;
         display: none;">
-            <div style="color: white">☕Loading...</div>
+            <div style="color: white"><img src="${logoPath}" style="min-width: 200px; max-width: 10vw; margin-right: 20px;" />Loading...</div>
         </div>
-    </div>`);
+    </div>`
+  );
 
-    // Attach functionality to the buttons
-    const domButtons = document.getElementById('bexioTimetrackingTemplates-entries')?.querySelectorAll("button.entry");
-    domButtons && domButtons.forEach(button => button.addEventListener('click', function (e) {
+  // Attach functionality to the buttons
+  const domButtons = document
+    .getElementById("bexioTimetrackingTemplates-entries")
+    ?.querySelectorAll("button.entry");
+  domButtons &&
+    domButtons.forEach((button) =>
+      button.addEventListener("click", function (e) {
         e.preventDefault();
-        fillForm(button.id)
+        fillForm(button.id);
 
         // Handle active template button
         const parent = (e.target as HTMLElement).parentNode as HTMLElement;
         const siblings = parent.querySelectorAll(".template-button");
 
         siblings.forEach((sibling) => {
-            sibling.classList.remove("template-button--active");
+          sibling.classList.remove("template-button--active");
         });
 
         (e.target as HTMLElement).classList.add("template-button--active");
-    }));
+      })
+    );
 
-    // Special action buttons
-    document.getElementById('AddNewTemplate')?.addEventListener('click', function (e) { e.preventDefault(); readFormData() });
-    document.getElementById('DeleteTemplate')?.addEventListener('click', function (e) { e.preventDefault(); confirmActiveTemplateDeletion() });
+  // Special action buttons
+  document
+    .getElementById("AddNewTemplate")
+    ?.addEventListener("click", function (e) {
+      e.preventDefault();
+      readFormData();
+    });
+  document
+    .getElementById("DeleteTemplate")
+    ?.addEventListener("click", function (e) {
+      e.preventDefault();
+      confirmActiveTemplateDeletion();
+    });
 }
 
 export default renderHtml;
