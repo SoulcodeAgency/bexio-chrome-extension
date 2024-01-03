768bf198dc8a3b8b7a5df1ccca9a3356099a3114
Fabian Gander <fabian.gander@soulcode.ch>
2024-01-03 13:22:24 +0100

    feat(Tabs) Keep last active tab state and enable it on load

--- packages/shared/chromeStorageSettings.ts
+++ packages/shared/chromeStorageSettings.ts
@@ -1,10 +1,22 @@
 import { load, save } from "./chromeStorage";
 export const applyNotesKey = "applyNotesSetting";
+export const activeTabIdKey = "activeTabId";
 
 export async function loadApplyNotesSetting(): Promise<boolean> {
-    const loadedImportData = await load<boolean>(applyNotesKey);
-    return loadedImportData ?? true;
+  const loadedImportData = await load<boolean>(applyNotesKey);
+  return loadedImportData ?? true;
 }
-export async function saveApplyNotesSetting(applyNotesSetting: boolean): Promise<any> {
-    return save<boolean>(applyNotesSetting, applyNotesKey);
+export async function saveApplyNotesSetting(
+  applyNotesSetting: boolean
+): Promise<any> {
+  return save<boolean>(applyNotesSetting, applyNotesKey);
+}
+
+export async function loadActiveTabId(): Promise<string | undefined> {
+  const loadedImportData = await load<string>(activeTabIdKey);
+  return loadedImportData ?? undefined;
+}
+
+export async function saveActiveTabId(tabId: string): Promise<any> {
+  return save<string>(tabId, activeTabIdKey);
 }
--- packages/sidePanel-import/src/App.tsx
+++ packages/sidePanel-import/src/App.tsx
@@ -4,20 +4,39 @@ import type { TabsProps } from "antd";
 import TemplateProvider from "./TemplateProvider";
 import TemplateEntries from "./components/TemplateEntries/TemplateEntries";
 import ImportEntries from "./components/ImportEntries/ImportEntries";
+import { useEffect, useState } from "react";
+import {
+  loadActiveTabId,
+  saveActiveTabId,
+} from "@bexio-chrome-extension/shared/chromeStorageSettings";
 
 function App() {
+  const [activeTabId, setActiveTabId] = useState("import");
   const items: TabsProps["items"] = [
     {
-      key: "1",
+      key: "import",
       label: `Import`,
       children: <ImportEntries />,
     },
     {
-      key: "2",
+      key: "templates",
       label: `Templates`,
       children: <TemplateEntries />,
     },
   ];
+
+  // Save active tab id when tabs change
+  const onTabChangeHandler = (activeKey: string) => {
+    setActiveTabId(activeKey);
+    saveActiveTabId(activeKey);
+  };
+
+  // Load active tab id on mount to keep the users last tab selection
+  useEffect(() => {
+    loadActiveTabId().then((data) => {
+      setActiveTabId(data ?? "import");
+    });
+  }, []);
 
   return (
     <TemplateProvider>
@@ -25,7 +44,12 @@ function App() {
         Bexio Timetracking Templates
       </h2>
       <div style={{ textAlign: "left" }}>
-        <Tabs defaultActiveKey="1" items={items} />
+        <Tabs
+          defaultActiveKey={activeTabId}
+          activeKey={activeTabId}
+          items={items}
+          onChange={onTabChangeHandler}
+        />
       </div>
     </TemplateProvider>
   );
