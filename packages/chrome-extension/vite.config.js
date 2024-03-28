62e0316a05dd7be16541bec7f2db363b92f62bb1
Fabian Gander <fabian.gander@soulcode.ch>
2024-03-28 22:14:44 +0100

    chore(config) Config updates

--- packages/chrome-extension/vite.config.js
+++ packages/chrome-extension/vite.config.js
@@ -7,25 +7,17 @@ export default ({ mode }) => {
     build: {
       assetsDir: "", // otherwise the scripts will be placed into the named assetsDir folder
       rollupOptions: {
-        // input: {
-        //   bexioTimetrackingTemplates:
-        //     "./src/apps/bexioTimetrackingTemplates/index.ts",
-        //   bexioProjectList: "./src/apps/bexioProjectList/index.ts",
-        // },
         output: {
           dir: "../../unpacked",
           // assetFileNames: "[name]",
           chunkFileNames: "[name].js",
           entryFileNames: "[name].js", // Removes the hash of the entry file
-          // assetFileNames: "[name].[format]",
-          // chunkFileNames: "[name].[format]",
-          // entryFileNames: "[name].[format]", // Removes the hash of the entry file
         },
       },
       exclude: [/\.html$/],
       outDir: "../../unpacked",
       emptyOutDir: true,
-      minify: true, // mode === "production",
+      minify: mode === "production",
     },
     plugins: [crx({ manifest })],
   });
