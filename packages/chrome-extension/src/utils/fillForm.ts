daaf26f9880fac1752e4dd79acce4d7cc9a346d0
Fabian Gander <fabian.gander@soulcode.ch>
2024-10-11 23:16:06 +0200

    chore(package) Package updates

--- packages/chrome-extension/src/utils/fillForm.ts
+++ packages/chrome-extension/src/utils/fillForm.ts
@@ -1,53 +1,65 @@
 import { chromeStorageTemplateEntries } from "@bexio-chrome-extension/shared";
 import { billableCheckbox } from "../selectors/billableCheckbox";
 import { contactField } from "../selectors/contactField";
-import { workFieldID, statusFieldID, contactPersonID, projectFieldID, packageFieldID, getLoader } from "../selectors/selectors";
+import {
+  workFieldID,
+  statusFieldID,
+  contactPersonID,
+  projectFieldID,
+  packageFieldID,
+  getLoader,
+} from "../selectors/selectors";
 import triggerCheckbox from "./triggerCheckbox";
 import triggerContactField from "./triggerContactField";
 import triggerField from "./triggerField";
 
 // Fix typescript error, for startViewTransition API
 interface CustomDocument extends Document {
-    startViewTransition: (callback: () => void) => void;
+  startViewTransition: (callback: () => void) => void;
 }
 
 function swapDisplayStyle(show = true) {
-    const loader = getLoader();
-    loader.style.display = show ? "flex" : "none";
+  const loader = getLoader();
+  loader.style.display = show ? "flex" : "none";
 }
 
 function toggleDisplayLoader(show = true) {
-    // Fallback for browsers that don't support this API:
-    if (!(document as CustomDocument).startViewTransition) {
-        swapDisplayStyle(show);
-        return;
-    }
-    // With a transition:
-    (document as CustomDocument).startViewTransition(() => swapDisplayStyle(show));
+  // Fallback for browsers that don't support this API:
+  if (!(document as CustomDocument).startViewTransition) {
+    swapDisplayStyle(show);
+    return;
+  }
+  // With a transition:
+  (document as CustomDocument).startViewTransition(() => swapDisplayStyle(show));
 }
 
 // Fill form
 async function fillForm(id: string, timeEntryBillable?: boolean) {
-    toggleDisplayLoader();
-    const templateEntries = await chromeStorageTemplateEntries.loadTemplates();
-    const entry = templateEntries.find(entry => entry.id === id);
-    const { contact, contactPerson = null, project = null, status = null, billable = true } = entry;
-    // Workaround because "package" is actually a reserved word
-    const packageValue = entry.package ?? null;
+  toggleDisplayLoader();
+  const templateEntries = await chromeStorageTemplateEntries.loadTemplates();
+  const entry = templateEntries.find((entry) => entry.id === id);
+  const { contact, contactPerson = null, project = null, status = null, billable = true } = entry;
+  // Workaround because "package" is actually a reserved word
+  const packageValue = entry.package ?? null;
 
-    await triggerField(workFieldID, "work");
-    await triggerField(statusFieldID, status);
+  await triggerField(workFieldID, "work");
+  await triggerField(statusFieldID, status);
 
-    // Project connections
-    await triggerContactField(contactField, contact);
-    await triggerField(contactPersonID, contactPerson);
-    await triggerField(projectFieldID, project);
+  // Project connections
+  await triggerContactField(contactField, contact);
+  await triggerField(contactPersonID, contactPerson);
+  await triggerField(projectFieldID, project);
 
-    await triggerField(packageFieldID, packageValue);
-    // If the time entry has a billable flag (timeEntryBillable), we prefer that one over the templates billable flag
-    await triggerCheckbox(billableCheckbox, timeEntryBillable ?? billable);
+  await triggerField(packageFieldID, packageValue);
+  // If the time entry has a billable flag (timeEntryBillable), we prefer that one over the templates billable flag
+  await triggerCheckbox(billableCheckbox, timeEntryBillable ?? billable);
 
-    toggleDisplayLoader(false);
+  toggleDisplayLoader(false);
+
+  // Focus the submit button, so the user can submit the form with the enter key directly if wanted
+  const form = document.getElementById("MonitoringForm");
+  const submitButton = form.getElementsByClassName("save")[0] as HTMLButtonElement;
+  submitButton.focus();
 }
 
 export default fillForm;
--- packages/sidePanel-import/src/utils/applyTemplate.ts
+++ packages/sidePanel-import/src/utils/applyTemplate.ts
@@ -1,27 +1,28 @@
 import { TemplateExchangeData } from "@bexio-chrome-extension/shared/types";
 
 function applyTemplate(templateId: string, timeEntryBillable?: boolean) {
-    (async () => {
-        const [tab] = await chrome.tabs.query({
-            active: true,
-            lastFocusedWindow: true,
-        });
-        if (tab.id) {
-            const data: TemplateExchangeData = {
-                mode: "template",
-                templateId,
-                timeEntryBillable
-            };
+  (async () => {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true,
+    });
+    if (tab.id) {
+      const data: TemplateExchangeData = {
+        mode: "template",
+        templateId,
+        timeEntryBillable,
+      };
 
-            console.log("Sending template data:", data);
-            // const response = 
-            await chrome.tabs.sendMessage(tab.id, data);
-            // do something with response here, not outside the function
-            // console.log(response);
-        } else {
-            throw new Error("No tab found");
-        }
-    })();
+      console.log("Sending template data:", data);
+      // const response =
+      await chrome.tabs.sendMessage(tab.id, data);
+      // do something with response here, not outside the function
+      // console.log(response);
+      chrome.tabs.update(tab.id, { active: true });
+    } else {
+      throw new Error("No tab found");
+    }
+  })();
 }
 
 export default applyTemplate;
