7de42c95fdbdf9c10e442874636bf8b532c82c07
Fabian Gander <fabian.gander@soulcode.ch>
2026-01-05 23:54:19 +0100

    fix(build) Back at working versions

--- packages/sidePanel-import/vite.config.ts
+++ packages/sidePanel-import/vite.config.ts
@@ -11,8 +11,8 @@ export default ({ mode }) => {
     resolve: {
       alias: {
         '~': path.resolve(__dirname, 'src'),
-        'react': path.resolve(__dirname, 'node_modules/react'),
-        'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+        'react': path.resolve(__dirname, '../../node_modules/react'),
+        'react-dom': path.resolve(__dirname, '../../node_modules/react-dom'),
       },
       dedupe: ['react', 'react-dom'],
     },
