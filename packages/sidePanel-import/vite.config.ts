236c8586f6a78122d6102d04398ecb05141d2db1
Fabian Gander <fabian.gander@soulcode.ch>
2026-01-06 00:02:22 +0100

    fix(build) Upgrade at working versions

--- packages/sidePanel-import/vite.config.ts
+++ packages/sidePanel-import/vite.config.ts
@@ -1,36 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   return defineConfig({
-
     plugins: [react()],
     base: "/sidePanel-import/",
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, 'src'),
-        'react': path.resolve(__dirname, '../../node_modules/react'),
-        'react-dom': path.resolve(__dirname, '../../node_modules/react-dom'),
+        "~": path.resolve(__dirname, "src"),
+        react: path.resolve(__dirname, "../../node_modules/react"),
+        "react-dom": path.resolve(__dirname, "../../node_modules/react-dom"),
       },
-      dedupe: ['react', 'react-dom'],
+      dedupe: ["react", "react-dom"],
     },
     build: {
-      outDir: '../../unpacked/sidePanel-import',
+      outDir: "../../unpacked/sidePanel-import",
       emptyOutDir: true,
       rollupOptions: {
         output: {
           manualChunks: undefined,
         },
       },
-      minify: mode === 'production',
+      minify: mode === "production",
     },
     server: {
       fs: {
         strict: false,
-        allow: [path.resolve(__dirname, '../shared')]
-      }
+        allow: [path.resolve(__dirname, "../shared")],
+      },
     },
-  })
-}
+  });
+};
